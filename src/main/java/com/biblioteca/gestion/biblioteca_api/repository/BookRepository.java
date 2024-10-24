99e73b383c3af46953ee969766765ffaa1f273a3
Leonel Ruiz Notari <leonelruiz692@gmail.com>
2024-10-24 17:12:57 -0300

    Update BookRepository.java
    Agregado metodos de busqueda por ID y busqueda por autor

--- src/main/java/com/biblioteca/gestion/biblioteca_api/repository/BookRepository.java
+++ src/main/java/com/biblioteca/gestion/biblioteca_api/repository/BookRepository.java
@@ -1,8 +1,16 @@
 package com.biblioteca.gestion.biblioteca_api.repository;
 
 import com.biblioteca.gestion.biblioteca_api.model.Book;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-}
 
+    @SuppressWarnings("null")
+    Optional<Book> findById(Long id);
+
+    List<Book> findByAuthor(String author);
+}
